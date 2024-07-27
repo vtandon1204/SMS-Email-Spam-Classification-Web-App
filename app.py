d4e86f71861006add7e7c383dd13629f0c133824
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-07-27 17:10:37 +0530

    commit

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ if st.button('Predict'):
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
-        st.header("Spam")
+        st.header("Spam !!!")
     else:
-        st.header("Not Spam")
+        st.header("Not Spam !!!")
